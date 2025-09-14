0416b7f701ced6c8bf39197d67a845da3b2e2f82
Max Netterberg <max.netterberg@gmail.com>
2025-09-14 13:50:01 +0200

    add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
 	plugins: [tailwindcss(), react()],
 
@@ -28,6 +27,11 @@ export default defineConfig(async () => ({
 		watch: {
 			// 3. tell vite to ignore watching `src-tauri`
 			ignored: ["**/src-tauri/**"],
+		},
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(import.meta.dirname, "./src"),
 		},
 	},
 }));
