60d2618bd2852043ac78083ad894510aa064532f
Max Netterberg <max.netterberg@gmail.com>
2025-10-07 18:59:15 +0200

    add solidjs deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import devtools from "solid-devtools/vite";
 import { defineConfig } from "vite";
-import 'vitest/config'
+import solidPlugin from "vite-plugin-solid";
+
+import "vitest/config";
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig(async () => ({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), solidPlugin(), devtools()],
 
   test: {
-    include: ['src/**/*.test.ts']
+    include: ["src/**/*.test.ts"],
   },
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
