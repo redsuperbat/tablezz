416286960e5998b1ead47f9584bf1a3d2adf4bdd
Max Netterberg <max.netterberg@gmail.com>
2025-12-01 21:59:12 +0100

    fix table structure query

--- src/useTableStructure.ts
+++ src/useTableStructure.ts
@@ -62,41 +62,55 @@ export function useTableStructure(tableName: () => string) {
   const { schema } = useSchemaContext();
   const database = useDatabase();
 
-  return useQuery<TableStructure[]>(() => ({
+  const primaryKeysQuery = useQuery(() => ({
     queryFn: async () => {
       const response = await database.select<
         {
           column_name: string;
-          data_type: PostgresDataType;
-          is_primary: boolean;
         }[]
       >(
         `
-SELECT
-  c.column_name,
-  c.data_type,
-  (tc.constraint_type = 'PRIMARY KEY') AS is_primary
-FROM information_schema.columns c
-LEFT JOIN information_schema.key_column_usage kcu
-  ON c.table_name = kcu.table_name
-  AND c.column_name = kcu.column_name
-  AND c.table_schema = kcu.table_schema
-LEFT JOIN information_schema.table_constraints tc
-  ON tc.constraint_name = kcu.constraint_name
-  AND tc.table_schema = kcu.table_schema
-  AND tc.constraint_type = 'PRIMARY KEY'
-WHERE c.table_schema = '${schema()}'
-  AND c.table_name = '${tableName()}'
-ORDER BY c.ordinal_position;
-`,
+          SELECT c.column_name
+          FROM information_schema.table_constraints tc
+          JOIN information_schema.constraint_column_usage AS ccu USING (constraint_schema, constraint_name) 
+          JOIN information_schema.columns AS c ON c.table_schema = tc.constraint_schema
+            AND tc.table_name = c.table_name AND ccu.column_name = c.column_name
+          WHERE constraint_type = 'PRIMARY KEY' and tc.table_name = '${tableName()}';`,
       );
 
       return response.map((r) => ({
         columnName: r.column_name,
-        dataType: r.data_type,
-        isPrimary: r.is_primary,
       }));
     },
-    queryKey: ["table-structure", schema(), tableName()],
+
+    queryKey: ["primary-keys", tableName()],
+  }));
+
+  return useQuery<TableStructure[]>(() => ({
+    enabled: !!primaryKeysQuery.data,
+    queryFn: async () => {
+      const primaryKeys = primaryKeysQuery.data ?? [];
+      const response = await database.select<
+        { column_name: string; data_type: PostgresDataType }[]
+      >(
+        `
+          SELECT column_name, data_type
+          FROM information_schema.columns
+          WHERE table_schema = '${schema()}' AND table_name = '${tableName()}';`,
+      );
+
+      return response.map((r) => {
+        const isPrimary = primaryKeys.some(
+          (k) => k.columnName === r.column_name,
+        );
+
+        return {
+          columnName: r.column_name,
+          dataType: r.data_type,
+          isPrimary,
+        };
+      });
+    },
+    queryKey: ["table-structure", schema(), tableName(), primaryKeysQuery.data],
   }));
 }
