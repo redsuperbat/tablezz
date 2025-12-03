3c52c307fbe1ef645e7bbdf17bb1184895b517c7
Max Netterberg <max.netterberg@gmail.com>
2025-12-03 21:45:37 +0100

    remove some padding

--- src/keybinds/KeybindHelp.tsx
+++ src/keybinds/KeybindHelp.tsx
@@ -46,7 +46,7 @@ export function KeybindHelp() {
                       {bind.bind}
                     </td>
                     <td class="w-8 px-2 py-2 text-center text-zinc-500">→</td>
-                    <td class="px-3 py-2 text-zinc-500">
+                    <td class="px-2 py-1 text-zinc-500">
                       <div class="flex flex-col">
                         <span>{bind.command}</span>
                         <small>{bind.description}</small>
